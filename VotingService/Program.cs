5bef0f250cdf2196bb5408e58b655b7deb23994b
mfd <mfd@1.com>
2017-01-22 19:55:01 +0000

    ori

--- VotingService/Program.cs
+++ VotingService/Program.cs
@@ -20,9 +20,6 @@ namespace VotingService
                 // Registering a service maps a service type name to a .NET type.
                 // When Service Fabric creates an instance of this service type,
                 // an instance of the class is created in this host process.
-                ////
-                ////
-                //ok011
 
                 ServiceRuntime.RegisterServiceAsync("VotingServiceType",
                     context => new VotingService(context)).GetAwaiter().GetResult();
